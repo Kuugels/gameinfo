83646e2139b1b8e598aa556c5f25d216be4fe5ca
Smuli <samuli.kakonen@metropolia.fi>
2018-02-20 23:18:58 +0200

    Changes to css and columns are now created dynamically

--- public/showdata.js
+++ public/showdata.js
@@ -1,8 +1,28 @@
 
 $(document).ready(function() {
+  createColumns();
+  createClickHandlers();
 
 });
 
-function setBackground() {
+function createColumns() {
+  var col = "";
+  for (var i = 1; i < 5; i++) {
+    col += "<div class='col-sm-3 column' id='col" + i + "'></div>";
+  }
+  $("#row").html(col);
+  for (var i = 1; i < 5; i++) {
+    $("#col"+i).css("background-image", "url('./images/img" + i + ".jpg')");
+  }
+}
 
+function createClickHandlers() {
+  $(".col-sm-3").click(function() {
+    $(this)
+      .removeClass("col-sm-3 col-sm-2")
+      .addClass("col-sm-6")
+      .siblings()
+        .removeClass("col-sm-3 col-sm-6")
+        .addClass('col-sm-2');
+  });
 }
